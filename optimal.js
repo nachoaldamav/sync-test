7839c2f8483de73d8de7e2c6e627785e4274a06b
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2022-12-11 23:07:24 +0100

    Update

--- optimal.js
+++ optimal.js
@@ -5,6 +5,7 @@ import { linkDirAsync, linkDirSync } from "./functions/link.js";
 const { platform } = os;
 
 async function main() {
+  console.log(`Running on ${platform()}...`);
   // If Mac or Windows, run the async tests
   if (platform() === "darwin" || platform() === "win32") {
     console.log("Cloning directory asyncronously...");
